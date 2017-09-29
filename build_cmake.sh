e566d1a9fd33814ef8f1cfa1a96596dc19bc848b
Bogdan Voloshin <ya.bovi2012@yandex.ru>
2017-09-29 12:53:53 +0300

    Fix library installation

--- build_cmake.sh
+++ build_cmake.sh
@@ -2,7 +2,7 @@ mkdir -p _build
 
 cd _build
 
-cmake -G "Unix Makefiles" -D CMAKE_INSTALL_PREFIX=/home/student/test_local ../IT-Cross-2017-Task1/Derivator/DerivatorLib/
+cmake -G "Unix Makefiles" -D CMAKE_INSTALL_PREFIX=/home/student/.local ../Derivator/DerivatorLib/
 
 make
 
