894dfe2be9c02cedc91ca3d26f441e73fd3d13e7
Bogdan Voloshin <ya.bovi2012@yandex.ru>
2017-09-29 12:46:32 +0300

    Add library installing

--- build_cmake.sh
+++ build_cmake.sh
@@ -1,3 +1,9 @@
-mkdir -p build
-cd build
-cmake -G "CodeBlocks - Unix Makefiles" ../Derivator/
+mkdir -p _build
+
+cd _build
+
+cmake -G "Unix Makefiles" -D CMAKE_INSTALL_PREFIX=/home/student/test_local ../IT-Cross-2017-Task1/Derivator/DerivatorLib/
+
+make
+
+cmake -D COMPONENT=developer -P cmake_install.cmake
